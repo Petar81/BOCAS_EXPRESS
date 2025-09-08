9deafe9fa2494cf799385c03a92f7da059f743b7
Petar81 <baki_pfc@yahoo.com>
2025-09-07 22:01:14 -0500

    rewrite MyHomePage

--- lib/main.dart
+++ lib/main.dart
@@ -33,21 +33,25 @@ class MyAppState extends ChangeNotifier {
   }
 }
 
+
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current;                 // ← Add this.
 
     return Scaffold(
       body: Column(
-        children: [Text('A random AWESOME idea:'), 
-        Text(appState.current.asLowerCase), 
-        ElevatedButton(
+        children: [
+          Text('A random AWESOME idea:'),
+          Text(pair.asLowerCase),                // ← Change to this.
+          ElevatedButton(
             onPressed: () {
               appState.getNext();
             },
             child: Text('Next'),
-          ),],
+          ),
+        ],
       ),
     );
   }
