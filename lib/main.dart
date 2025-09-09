a8ebd1de917384f6f995e6ab9b94fb04e9e86a55
Petar81 <baki_pfc@yahoo.com>
2025-09-09 00:26:32 -0500

    update TextTheme

--- lib/main.dart
+++ lib/main.dart
@@ -68,6 +68,9 @@ class BigCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);  
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
     return Card(
       color: theme.colorScheme.primary,
       child: Padding(
