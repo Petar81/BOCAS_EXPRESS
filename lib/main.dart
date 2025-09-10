5726d886f9a2d2364a7324a9d4bccb8486768298
Petar81 <baki_pfc@yahoo.com>
2025-09-10 14:44:26 -0500

    convert MyHomePage to a StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -46,7 +46,12 @@ var favorites = <WordPair>[];
 }
 
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
